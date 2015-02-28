14e53a7cc0c7da209a8411daa717b93e1762bf0d
Katz Ueno <katz@concrete5.co.jp>
2015-02-28 10:27:36 +0900

    Fix Image Slider

--- application/blocks/image_slider/view.php
+++ application/blocks/image_slider/view.php
@@ -1,40 +1,33 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 $navigationTypeText = ($navigationType == 0) ? 'arrows' : 'pages';
-$c = Page::getCurrentPage();
-if ($c->isEditMode()) { ?>
-    <div id="orbit">
-        <div class="ccm-edit-mode-disabled-item" style="width: <?php echo $width; ?>; height: <?php echo $height; ?>">
-            <div style="padding: 40px 0px 40px 0px"><?php echo t('Image Slider disabled in edit mode.')?></div>
+$c = Page::getCurrentPage();?>
+<div id="orbit">
+  <?php if(count($rows) > 0) { ?>
+    <?php foreach($rows as $row) { ?>
+        <div>
+            <?php if($row['linkURL']) { ?>
+                <a href="<?php echo $row['linkURL'] ?>" class="mega-link-overlay">
+            <?php } ?>
+            <?php
+            $f = File::getByID($row['fID'])
+            ?>
+            <?php if(is_object($f)) {
+                $tag = Core::make('html/image', array($f, false))->getTag();
+                if($row['title']) {
+                	$tag->alt($row['title']);
+                }else{
+                	$tag->alt("slide");
+                }
+                print $tag; ?>
+            <?php } ?>
+            <?php if($row['linkURL']) { ?>
+                </a>
+            <?php } ?>
         </div>
-    </div>
-<?php  } else { ?>
-    <div id="orbit">
-        <?php foreach($rows as $row) { ?>
-            <div>
-                <?php if($row['linkURL']) { ?>
-                    <a href="<?php echo $row['linkURL'] ?>" class="mega-link-overlay">
-                <?php } ?>
-                <?php
-                $f = File::getByID($row['fID'])
-                ?>
-                <?php if(is_object($f)) {
-                    $tag = Core::make('html/image', array($f, false))->getTag();
-                    if($row['title']) {
-                    	$tag->alt($row['title']);
-                    }else{
-                    	$tag->alt("slide"); 
-                    }
-                    print $tag; ?>
-                <?php } ?>
-                <?php if($row['linkURL']) { ?>
-                    </a>
-                <?php } ?>
-            </div>
-        <?php } else { ?>
-            <div class="ccm-image-slider-placeholder">
-                <div style="padding: 40px 0px 40px 0px"><?php echo t('Image Slider disabled in edit mode.')?>
-                </div>
-            </div>
-        <?php } ?>
-    </div>
-<?php } ?>
+      <?php } ?>
+    <?php } else { ?>
+      <div class="ccm-edit-mode-disabled-item" style="width: 100%; height: 200px">
+        <p><?php echo t('No Slides Entered.'); ?></p>
+      </div>
+    <?php } ?>
+</div>
