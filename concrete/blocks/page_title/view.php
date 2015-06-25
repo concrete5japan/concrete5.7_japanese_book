2d49f8ef276795550f422eaf6e1998570eef1568
Katz Ueno <katz@concrete5.co.jp>
2015-06-25 23:16:36 +0900

    Delete all unnecessary files

--- concrete/blocks/page_title/view.php
+++
@@ -1,2 +0,0 @@
-<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
-<h1 class="page-title"><?php echo $title?></h1>
--- concrete/themes/concrete/page_theme.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Concrete\Theme\Concrete;
-class PageTheme extends \Concrete\Core\Page\Theme\Theme {
-
-	public function registerAssets() {
-		$this->providesAsset('css', 'core/frontend/*');
-	}
-
-}
